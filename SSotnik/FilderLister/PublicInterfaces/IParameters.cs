ac0257389c53b7cd308a674522681588490922f1
sonicpro <sonic@ukrpost.net>
2016-05-17 17:02:32 +0300

    Initial commit.

---
+++ SSotnik/FilderLister/PublicInterfaces/IParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilderLister.PublicInterfaces
+{
+    // Sets Processor so that its "ShowContents" handler directed to the correct folder.
+    interface IParameters
+    {
+        string GetPath();
+    }
+}
